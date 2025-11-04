1ce48d0b390d28a1b92fc04fd121bd700e71782a
Elcucuy420 <luengogabriel.9@gmail.com>
2025-11-04 09:58:07 +0100

    ci: add research-publish workflow (6h cron)

--- shorts-autopilot/shorts_autopilot/uploader/tiktok_uploader.py
+++
@@ -1,7 +0,0 @@
-import os
-SESSION = os.getenv('TIKTOK_SESSION_ID')
-
-def upload_short_tiktok(path: str, title: str, tags=None):
-    if not SESSION:
-        return {'ok': False, 'reason': 'missing TIKTOK_SESSION_ID'}
-    return {'ok': False, 'reason': 'uploader not implemented'}
